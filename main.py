86ade782cd476c443f18f6f5b64f4520c08911ca
Mauro Júnio <maurohunt@hotmail.com>
2022-06-18 13:11:45 -0300

    change nomeclature

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ import requests
 GUI = Builder.load_file("screen.kv")
 
 
-class MeuAplicativo(App):
+class MyApp(App):
     def build(self):
         return GUI
 
     def on_start(self):
-        self.root.ids["moeda1"].text = f"Dólar: R${self.pegar_cotacao('USD')}" 
-        self.root.ids["moeda2"].text = f"Euro: R${self.pegar_cotacao('EUR')}"
-        self.root.ids["moeda3"].text = f"Bitcoin: R${self.pegar_cotacao('BTC')}"
-        self.root.ids["moeda4"].text = f"Ethereum: R${self.pegar_cotacao('ETH')}"
+        self.root.ids["moeda1"].text = f"Dólar: R${self.get_price('USD')}" 
+        self.root.ids["moeda2"].text = f"Euro: R${self.get_price('EUR')}"
+        self.root.ids["moeda3"].text = f"Bitcoin: R${self.get_price('BTC')}"
+        self.root.ids["moeda4"].text = f"Ethereum: R${self.get_price('ETH')}"
 
-    def pegar_cotacao(self, moeda):
+    def get_price(self, moeda):
         link = f"https://economia.awesomeapi.com.br/last/{moeda}-BRL"
         requisicao = requests.get(link)
-        dic_requisicao = requisicao.json()
-        cotacao = dic_requisicao[f"{moeda}BRL"]["bid"]
-        return cotacao
+        dic_request = requisicao.json()
+        price = dic_request[f"{moeda}BRL"]["bid"]
+        return price
 
 
-MeuAplicativo().run()
+MyApp().run()
